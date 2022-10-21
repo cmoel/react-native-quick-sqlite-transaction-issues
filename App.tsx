5c96a8c28a26ef7251f870a19e53a3950e2cfc0a
Christopher Moeller <cmoel@me.com>
2022-10-21 14:24:13 -0400

    Open an example database

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import * as QuickSQLite from 'react-native-quick-sqlite';
+
+import type {QuickSQLiteConnection} from 'react-native-quick-sqlite';
+
+type DatabaseConnection = QuickSQLiteConnection | null;
 
 function App() {
+  let dbName = 'example.db';
+  let [db, setDb] = useState<DatabaseConnection>(null);
+
+  useEffect(() => {
+    if (db) return;
+
+    let result = QuickSQLite.open({name: dbName});
+    setDb(result);
+  }, [dbName]);
+
+  console.log(db);
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#222222'}}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
