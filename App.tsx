00ddd613ad85786eed93bc8b67467e70412f4267
Christopher Moeller <cmoel@me.com>
2022-10-21 14:42:01 -0400

    Add buttons with migration and select all functionality

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import {useEffect, useState} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {ReactNode, useEffect, useState} from 'react';
+import {Pressable, SafeAreaView, Text, View} from 'react-native';
 import * as QuickSQLite from 'react-native-quick-sqlite';
 
+import type {GestureResponderEvent, StyleProp, ViewStyle} from 'react-native';
 import type {QuickSQLiteConnection} from 'react-native-quick-sqlite';
 
 type DatabaseConnection = QuickSQLiteConnection | null;
@@ -9,6 +10,7 @@ type DatabaseConnection = QuickSQLiteConnection | null;
 function App() {
   let dbName = 'example.db';
   let [db, setDb] = useState<DatabaseConnection>(null);
+  let [migrated, setMigrated] = useState(false);
 
   useEffect(() => {
     if (db) return;
@@ -17,16 +19,84 @@ function App() {
     setDb(result);
   }, [dbName]);
 
-  console.log(db);
+  function migrateDatabaseUp() {
+    if (!db) return;
+    if (migrated) return;
+
+    let createPeople = `create table if not exists
+                          people (
+                            id integer primary key autoincrement,
+                            name text not null
+                          )`;
+    let createDogs = `create table if not exists
+                        dogs (
+                          id integer primary key autoincrement,
+                          person_id integer not null,
+                          name text not null
+                        )`;
+
+    db.execute(createPeople);
+    db.execute(createDogs);
+    setMigrated(true);
+  }
+
+  function printDatabaseContents() {
+    if (!db) return;
+    if (!migrated) return;
+
+    let selectAll = `select
+                       people.id as person_id,
+                       dogs.id as dog_id,
+                       people.name as person,
+                       dogs.name as dog
+                     from people
+                     inner join dogs
+                       on people.id = dogs.person_id`;
+    db.executeAsync(selectAll).then(res => console.log('RES', res.rows));
+  }
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#222222'}}>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Button
+          onPress={() => migrateDatabaseUp()}
+          style={{padding: 16, marginVertical: 8}}>
+          <Text style={{color: '#dddddd', textAlign: 'center'}}>
+            Migrate database up
+          </Text>
+        </Button>
+        <Button
+          onPress={() => printDatabaseContents()}
+          style={{padding: 16, marginVertical: 8}}>
+          <Text style={{color: '#dddddd', textAlign: 'center'}}>
+            Log database contents to console
+          </Text>
+        </Button>
+      </View>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text style={{color: '#cccccc'}}>
           React Native Quick SQLite example app
         </Text>
       </View>
     </SafeAreaView>
+  );
+}
+
+type ButtonProps = {
+  children: ReactNode;
+  onPress: (event: GestureResponderEvent) => void;
+  style?: StyleProp<ViewStyle>;
+};
+function Button({children, onPress, style = {}}: ButtonProps) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({pressed}) => [
+        {backgroundColor: pressed ? '#444444' : '#333333'},
+        style,
+      ]}>
+      {children}
+    </Pressable>
   );
 }
 
